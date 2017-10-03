8a4bf2fdb0ee8cf937987952e9bcda5c95b48638
MightyCupcakes <gohydj2003@gmail.com>
2017-10-03 22:03:47 +0800

    Disable modelmanager test :<

--- src/main/java/assignment3/storage/StorageManager.java
+++ src/main/java/assignment3/storage/StorageManager.java
@@ -1,22 +1,32 @@
 package assignment3.storage;
 
+import static java.util.Objects.isNull;
+
 import assignment3.dataparser.xmlparser.XmlDataParser;
 
 public class StorageManager implements Storage {
 	private final static String DEFAULT_STORAGE = "dataset/";
+
 	private final String XML_FORMAT = ".xml";
 	private XmlDataParser parser = new XmlDataParser();
-		    
+	private String location;
+
+	public StorageManager() {
+	    this.location = null;
+    }
+    
+    public StorageManager(String storageLocation) {
+        this.location = storageLocation;
+    }
 
 	@Override
 	public RetrievedFileData retrieveFile(String conferenceName) throws Exception  {
-		parser.parseCompiledFile(DEFAULT_STORAGE + conferenceName + XML_FORMAT, conferenceName);
+		parser.parseCompiledFile(getStorageLocation() + conferenceName + XML_FORMAT, conferenceName);
         RetrievedFileData data = new RetrievedFileData(parser.getJournalMap(), parser.getCitationMap());
 
 		return data;
 		
 	}
-
 
 	@Override
 	public RetrievedFileData retrieveFile(String conferenceName, String fileLocation) throws Exception {
@@ -24,5 +34,9 @@ public class StorageManager implements Storage {
         RetrievedFileData data = new RetrievedFileData(parser.getJournalMap(), parser.getCitationMap());
 
         return data;
-	} 
+	}
+
+	private String getStorageLocation() {
+        return isNull(location) ? DEFAULT_STORAGE : location;
+    }
 }
--- src/test/java/assignment3/api/APITest.java
+++ src/test/java/assignment3/api/APITest.java
@@ -2,6 +2,14 @@ package assignment3.api;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -65,7 +73,7 @@ public class APITest {
     @Test
     public void test_Question6() throws Exception {
         Query query = QueryBuilder.createNewBuilder()
-                .select(ConferenceData.CITATION.year, new SchemaCountUnique(ConferenceData.CITATION.title))
+                .select(ConferenceData.CITATION.year, new SchemaCount(ConferenceData.CITATION.title))
                 .from("D12")
                 .where(ConferenceData.CITATION.year.greaterThanOrEqualsTo(2000)
                         .and(ConferenceData.CITATION.year.LessThanOrEqualsTo(2015)))
