d1ffda01f5dc353e874dc6756f4bc0a5bf11950e
tanboonjoon <tboonjoon@gmail.com>
2017-10-04 17:25:49 +0800

    add test for logic

--- src/test/java/assignment3/logic/LogicManagerTest.java
+++ src/test/java/assignment3/logic/LogicManagerTest.java
@@ -10,6 +10,9 @@ import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableList;
+
+import assignment3.datarepresentation.SerializedCitation;
 import assignment3.datarepresentation.SerializedJournalCitation;
 import assignment3.model.ModelManager;
 
@@ -49,6 +52,7 @@ public class LogicManagerTest {
     	assertEquals(journalCitation_two.journal.title, "Dtest_2 title");
         assertEquals(journalCitation_two.journal.author, "Dtest_2 author");
         
+
         
 	}
 	
@@ -65,8 +69,44 @@ public class LogicManagerTest {
 		
 		assertEquals(journalCitation_one.journal, journalCitation_two.journal);
 		assertEquals(journalCitation_three.journal, journalCitation_four.journal);
+		
+        List<SerializedCitation> expectedCitations = ImmutableList.<SerializedCitation>builder()
+                .add(new SerializedCitation.Builder()
+                        .withAuthor("A Abeill")
+                        .withTitle("Treebanks: Building and Using Parsed Corpora.")
+                        .withYear("2003").build())
+                .add(new SerializedCitation.Builder()
+                        .withTitle("Painless unsupervised learning with features.")
+                        .withYear("2010")
+                        .withAuthor("Taylor Berg-Kirkpatrick")
+                        .withAuthor("Alexandre Bouchard")
+                        .withAuthor("John DeNero")
+                        .withAuthor("Dan Klein")
+                        .withBooktitle("In Proc. NAACL,").build()
+                ).build();
+        assertEquals(expectedCitations.get(0), journalCitation_one.citation);
+        assertEquals(expectedCitations.get(1), journalCitation_two.citation);
+        
+        List<SerializedCitation> expectedCitations_two = ImmutableList.<SerializedCitation>builder()
+                .add(new SerializedCitation.Builder()
+                        .withAuthor("D_test_citation_two_one_author")
+                        .withTitle("D_test_citation_two_one_title")
+                        .withYear("2003").build())
+                .add(new SerializedCitation.Builder()
+                        .withTitle("D_test_citation_two_two_title")
+                        .withYear("2010")
+                        .withAuthor("D_test_citation_two_two_author")
+                        .withAuthor("D_test_citation_two_two_author_two")
+                        .withAuthor("D_test_citation_two_two_author_three")
+                        .withAuthor("D_test_citation_two_two_author_four")
+                        .withBooktitle("In Proc. NAACL,").build()
+                ).build();
+        assertEquals(expectedCitations_two.get(0), journalCitation_three.citation);
+        assertEquals(expectedCitations_two.get(1), journalCitation_four.citation);
+
 
 	}
+
 	
 	@After
 	public void deleteFile() {
