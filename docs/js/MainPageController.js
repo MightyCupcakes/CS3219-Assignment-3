cc03ff5beaba8a579b7ea57a192e230391b0e5dc
MightyCupcakes <gohydj2003@gmail.com>
2017-11-02 20:55:45 +0800

    General improvements to main.html

--- docs/js/MainPageController.js
+++ docs/js/MainPageController.js
@@ -1,6 +1,22 @@
+function populatePresetsType() {
+    var select = $('#premade_type');
+
+    select.append($('<option>', { value: 1, text: 'Transition over time' }));
+    select.append($('<option>', { value: 2, text: 'Contemporary comparison' }));
+    select.append($('<option>', { value: 3, text: 'Top N X of Y' }));
+}
+
 $(document).ready (function () {
-  sendAjaxRequest("main.html", {data:"hi"}, "GET", 
-    function(data) { 
-      $('#viz').attr('src', data.src)
-    })
+    
+    populatePresetsType();
+
+    sendAjaxRequest("main.html", {data:"hi", data2:"hihi"}, "GET", 
+        function(data) { 
+            $('#viz').attr('src', data.src)
+     });
+
+
+  $( "#progressbar" ).progressbar({
+      value: false
+    });
 });
--- src/main/java/assignment3/webserver/handler/GetRequestHandler.java
+++ src/main/java/assignment3/webserver/handler/GetRequestHandler.java
@@ -1,8 +1,12 @@
 package assignment3.webserver.handler;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 
+import com.google.common.collect.ImmutableMap;
 import com.sun.net.httpserver.HttpExchange;
 
 import assignment3.webserver.exceptions.WebServerException;
@@ -11,18 +15,33 @@ import assignment3.webserver.exceptions.WebServerException;
  * Processes a GET request from the user and response with the appropriate HTML file to
  * display the visualisation
  */
-public class GetRequestHandler implements RequestHandler {
+public class GetRequestHandler extends FileRequestHandler {
 
     @Override
     public String handleRequest(HttpExchange httpExchange) throws WebServerException {
-        String queryString = httpExchange.getRequestURI().getQuery();
+        Map<String, String> keyValuePairs = parseQueryString(httpExchange.getRequestURI().getQuery());
 
+        if (!keyValuePairs.containsKey("clientonly")) {
+            // Create JSON response
+            JsonObjectBuilder builder = Json.createObjectBuilder();
+            // TODO: Not hardcode this
+            builder.add("src", "q1.html");
 
-        // Create JSON response
-        JsonObjectBuilder builder = Json.createObjectBuilder();
-        // TODO: Not hardcode this
-        builder.add("src", "q1.html");
+            return builder.build().toString();
+        } else {
+            return super.handleRequest(httpExchange);
+        }
+    }
 
-        return builder.build().toString();
+    private Map<String, String> parseQueryString(String queryString) {
+        ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+        String[] keyValuePairs = queryString.split("&");
+
+        for (String keyValuePair : keyValuePairs) {
+            String[] keyAndValue = keyValuePair.split("=");
+            builder.put(keyAndValue[0], keyAndValue[1]);
+        }
+
+        return builder.build();
     }
 }
