b71e76b2ada89a335207fa301a88e359fa86d0a9
MightyCupcakes <gohydj2003@gmail.com>
2017-11-07 16:14:15 +0800

    Add javadoc comments for RequestProcessor#handleRequest

--- src/main/java/assignment3/webserver/requestprocessor/RequestProcessor.java
+++ src/main/java/assignment3/webserver/requestprocessor/RequestProcessor.java
@@ -3,6 +3,9 @@ package assignment3.webserver.requestprocessor;
 import java.util.Map;
 
 public interface RequestProcessor {
-
+    /**
+     * Handles the request represented by the key-value pairs and returns a JSON string representing
+     * the response to be sent back to the user browser
+     */
     String handleRequest(Map<String, String> keyValuePairs);
 }
