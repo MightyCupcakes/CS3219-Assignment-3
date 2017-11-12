a5fca7f9c673d91d24adb50b1e441c8d10de3a57
MightyCupcakes <gohydj2003@gmail.com>
2017-11-12 14:26:46 +0800

    Complete CitationWebWebQueryProcessor

--- src/main/java/assignment3/webserver/webquery/CitationWebWebQueryProcessor.java
+++ src/main/java/assignment3/webserver/webquery/CitationWebWebQueryProcessor.java
@@ -1,30 +1,38 @@
 package assignment3.webserver.webquery;
 
+import static assignment3.webserver.WebServerConstants.PREMADE_QUERIES;
+
 import assignment3.webserver.WebServerManager;
 import assignment3.webserver.registry.RegisterProcessor;
 import assignment3.webserver.webrequest.WebRequest;
 
 @RegisterProcessor( requestType = "Citation Web")
 public class CitationWebWebQueryProcessor implements WebQueryProcessor {
-	
-	@Override
-	public boolean processAndSaveIntoCSV(WebServerManager manager, WebRequest webRequest) {
-		String premadeQuery = webRequest.getValue("premadeQuery");
-		System.out.println(premadeQuery);
-		if(premadeQuery == "Citation Web for base paper"){
-			CitationNetworkBasePaperWebQueryProcessor citationNetworkBasePaper = new CitationNetworkBasePaperWebQueryProcessor();
-			citationNetworkBasePaper.processAndSaveIntoCSV(manager, webRequest);
-		}
-		else if(premadeQuery == "Citation Web Forest in range of years"){
-			ForestWebQueryProcessor forest = new ForestWebQueryProcessor();
-			forest.processAndSaveIntoCSV(manager, webRequest);
-		}
-		return true;
-	}
 
-	@Override
-	public String getHtmlFileName() {
-		//need to return "Forest" or "CollapsibleTree"
-		return "CollapsibleTree";
-	}
+    private static final String CITATION_WEB = PREMADE_QUERIES.get(5).name;
+    private static final String CITATION_FOREST = PREMADE_QUERIES.get(6).name;
+
+    private WebQueryProcessor processor;
+
+    @Override
+    public boolean processAndSaveIntoCSV(WebServerManager manager, WebRequest webRequest) {
+        String premadeQuery = webRequest.getValue("premadeQuery");
+
+        if(CITATION_WEB.equalsIgnoreCase(premadeQuery)) {
+            processor = new CitationNetworkBasePaperWebQueryProcessor();
+
+        } else if(CITATION_FOREST.equalsIgnoreCase(premadeQuery)) {
+            processor = new ForestWebQueryProcessor();
+
+        } else {
+            return false;
+        }
+
+        return processor.processAndSaveIntoCSV(manager, webRequest);
+    }
+
+    @Override
+    public String getHtmlFileName() {
+        return processor.getHtmlFileName();
+    }
 }
